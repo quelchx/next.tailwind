09743908e5de52a1506dae2c3f467b1e8bc397c3
quelchx <quelchx@gmail.com>
2022-01-20 12:02:40 -0500

    minor correction

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function Application({ Component, pageProps }) {
     <section>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins&family=Ubuntu&display=swap"
           rel="stylesheet"
